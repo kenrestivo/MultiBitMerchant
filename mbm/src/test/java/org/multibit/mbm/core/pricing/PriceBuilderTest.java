762b74ad9e44e3cbd0b8c264d334c9ca44af4e3e
ken restivo <ken@restivo.org>
2013-03-04 00:12:23 -0800

    Upgrade to xchange 1.4.0 and bitcoinj 0.7.1

--- mbm/src/test/java/org/multibit/mbm/core/pricing/PriceBuilderTest.java
+++ mbm/src/test/java/org/multibit/mbm/core/pricing/PriceBuilderTest.java
@@ -1,6 +1,6 @@
 package org.multibit.mbm.core.pricing;
 
-import com.xeiam.xchange.utils.MoneyUtils;
+import com.xeiam.xchange.currency.MoneyUtils;
 import org.joda.money.BigMoney;
 import org.junit.Test;
 import org.multibit.mbm.core.model.*;
@@ -17,7 +17,7 @@ public class PriceBuilderTest {
     Role supplierRole = DatabaseLoader.buildSupplierRole();
     User steveUser = DatabaseLoader.buildSteveSupplier(supplierRole);
 
-    BigMoney unitPrice = MoneyUtils.parseFiat("GBP 1.23");
+    BigMoney unitPrice = MoneyUtils.parse("GBP 1.23");
 
     // Configure some PricingRules
     PricingRule pricingRule = DatabaseLoader.buildPresetMarginPricingRule();
